f227afd13fb55d0e2fdf24bd0853136464b1b5f6
sam <saminusama@gmail.com>
2023-12-17 18:00:12 -0500

    where is my post

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ PID=$! # record the PID
 
 # python -m unittest discover -s tests
 
-# # newman run try.postman_collection.json
-# newman run s01+forum-multiple_posts.postman_collection.json -e env.json
-# newman run s02+forum-post_read_delete.postman_collection.json -e env.json
-# newman run s03+forum-users.postman_collection.json -e env.json
+# newman run try.postman_collection.json
+newman run s01+forum-multiple_posts.postman_collection.json -e env.json
+newman run s02+forum-post_read_delete.postman_collection.json -e env.json
+newman run s03+forum-users.postman_collection.json -e env.json
 newman run s04+forum-posts_users.postman_collection.json
