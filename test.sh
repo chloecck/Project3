7613b15bf034633da1a5c777a08c698469a1caee
sam <saminusama@gmail.com>
2023-12-17 04:38:59 -0500

    py unit

--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
+python3 -m unittest discover -s tests
+
 # newman run try.postman_collection.json
 newman run s01+forum-multiple_posts.postman_collection.json -e env.json
 newman run s02+forum-post_read_delete.postman_collection.json -e env.json
