ddb0094ada15a6aabf3e8d7679310aa65c4a4543
sam <saminusama@gmail.com>
2023-12-17 02:18:14 -0500

    .json collection

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
-newman run s01+forum-multiple_posts.postman_collection -e env.json
-newman run s02+forum-post_read_delete.postman_collection -e env.jsom
-newman run s03+forum-users.postman_collection -e env.json
-newman run s04+forum-posts_users.postman_collection -e env.json
+newman run s01+forum-multiple_posts.postman_collection.json -e env.json
+newman run s02+forum-post_read_delete.postman_collection.json -e env.json
+newman run s03+forum-users.postman_collection.json -e env.json
+newman run s04+forum-posts_users.postman_collection.json -e env.json
