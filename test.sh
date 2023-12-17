ff5dfcd67fe3ded85877d8a403e935f443e6b24a
Chloe Chuqing Ke <cke1@stevens.edu>
2023-12-17 03:02:29 -0500

    rm node

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ trap 'kill $PID' EXIT # kill the server on exit
 ./run.sh &
 PID=$! # record the PID
 
-# newman run try.postman_collection.json
-# newman run s01+forum-multiple_posts.postman_collection.json -e env.json
-# newman run s02+forum-post_read_delete.postman_collection.json -e env.json
+newman run try.postman_collection.json
+newman run s01+forum-multiple_posts.postman_collection.json -e env.json
+newman run s02+forum-post_read_delete.postman_collection.json -e env.json
 newman run s03+forum-users.postman_collection.json -e env.json
 # newman run s04+forum-posts_users.postman_collection.json -e env.json
